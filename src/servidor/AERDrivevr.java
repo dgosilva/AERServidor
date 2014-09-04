858f46ed46583f3dd480c65ee3c71a52607d6772
dgosilva <davidgosilva@gmail.com>
2014-09-04 16:27:02 -0300

    adicionei dependencia para servidor da amazon, assim como os imports necessarios no servidor

--- src/servidor/AERDrivevr.java
+++ src/servidor/AERDrivevr.java
@@ -10,6 +10,20 @@ import org.unbiquitous.uos.core.messageEngine.dataType.UpDriver;
 import org.unbiquitous.uos.core.messageEngine.messages.Call;
 import org.unbiquitous.uos.core.messageEngine.messages.Response;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
 
 public class AERDrivevr implements UosDriver{
 
