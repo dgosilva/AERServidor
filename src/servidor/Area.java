831b4da7a3845835a94d64505850a49e3841522e
Filipe Lima <filipepontelima@gmail.com>
2014-09-07 12:27:53 -0300

    metodo de interseccao de areas

--- src/servidor/Area.java
+++ src/servidor/Area.java
@@ -64,4 +64,26 @@ public class Area {
 			return false;
 		}
 	}
+	
+	public double interseccaoArea(Area area) {
+		
+		Ponto centro1 = new Ponto(latitude, longitude);
+		Ponto centro2 = new Ponto(area.getLatitude() , area.getLongitude());
+		
+		Double r = raio;
+		Double R = area.getRaio();
+		Double d = centro1.distanciaMetro(centro2) ;
+		if(R < r){
+		    // swap
+		    r = area.getRaio();
+		    R = raio;
+		}
+		Double part1 = r*r*Math.acos((d*d + r*r - R*R)/(2*d*r));
+		Double part2 = R*R*Math.acos((d*d + R*R - r*r)/(2*d*R));
+		Double part3 = 0.5*Math.sqrt((-d+r+R)*(d+r-R)*(d-r+R)*(d+r+R));
+
+		Double intersectionArea = part1 + part2 - part3;
+		
+		return intersectionArea;
+	}
 }
