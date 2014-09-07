2f631397c01fd75117cffeaca38802f47cd373f8
Filipe Lima <filipepontelima@gmail.com>
2014-09-07 15:37:58 -0300

    Double para double

--- src/servidor/Area.java
+++ src/servidor/Area.java
@@ -70,19 +70,19 @@ public class Area {
 		Ponto centro1 = new Ponto(latitude, longitude);
 		Ponto centro2 = new Ponto(area.getLatitude() , area.getLongitude());
 		
-		Double r = raio;
-		Double R = area.getRaio();
-		Double d = centro1.distanciaMetro(centro2) ;
+		double r = raio;
+		double R = area.getRaio();
+		double d = centro1.distanciaMetro(centro2) ;
 		if(R < r){
 		    // swap
 		    r = area.getRaio();
 		    R = raio;
 		}
-		Double part1 = r*r*Math.acos((d*d + r*r - R*R)/(2*d*r));
-		Double part2 = R*R*Math.acos((d*d + R*R - r*r)/(2*d*R));
-		Double part3 = 0.5*Math.sqrt((-d+r+R)*(d+r-R)*(d-r+R)*(d+r+R));
+		double part1 = r*r*Math.acos((d*d + r*r - R*R)/(2*d*r));
+		double part2 = R*R*Math.acos((d*d + R*R - r*r)/(2*d*R));
+		double part3 = 0.5*Math.sqrt((-d+r+R)*(d+r-R)*(d-r+R)*(d+r+R));
 
-		Double intersectionArea = part1 + part2 - part3;
+		double intersectionArea = part1 + part2 - part3;
 		
 		return intersectionArea;
 	}
