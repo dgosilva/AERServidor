8c00dc26aa62c27862f50cd042eab5f0e1a9f57d
dgosilva <davidgosilva@gmail.com>
2014-09-08 09:21:28 -0300

    manifest e tentativa de arrumar a main

--- src/servidor/Main.java
+++ src/servidor/Main.java
@@ -1,12 +1,18 @@
 package servidor;
 
 import org.unbiquitous.network.http.connection.ServerMode;
+import org.unbiquitous.uos.core.InitialProperties;
 import org.unbiquitous.uos.core.UOS;
 
 public class Main {
 
 	public static void main(String[] args){
 		UOS uos = new UOS();
+		
+//		InitialProperties config = new ServerMode.Properties();
+//		config.addDriver(AERDrivevr.class);
+//		uos.start(config);
+		
 	    uos.start(new ServerMode.Properties());
 	}
 	
